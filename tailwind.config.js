5a10563572a888afe4aeae8213224dc799cc5c18
Shipon Hossen Raju <msshipon234@gmail.com>
2022-09-20 06:41:15 +0600

    Some Problem fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: [],
+  content: ["./src/**/*.{html,js}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
@@ -8,4 +8,3 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
